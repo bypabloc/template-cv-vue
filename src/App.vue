ad3194349e2655af2cba38cfa1166cfeb9dc3619
Pablo Contreras <pacg1991@gmail.com>
2021-05-30 13:13:40 -0500

    bootstrap n first element

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,69 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <!-- <div id="wrapper" class="mh-100">
+        <div class="container">
+        </div>
+    </div> -->
+    <div class="container fluid fill-height" style="background-color: rgba(0,0,255,.1);">
+
+        <main class="col-12 mt-3 flex-fill">
+            <div class="row p-3">
+                <div class="col-4">
+                    izq
+                </div>
+                <div class="col-8">
+                    der
+                </div>
+            </div>
+        </main>
+
+        <Home/>
+        <!-- <nav class="navbar navbar-expand-sm navbar-light flex-shrink-0">
+            <a class="brand navbar-brand" href="/">Brand</a>
+            <button class="navbar-toggler s-btn-hamburger order-first s-color-icons" aria-expanded="true" aria-controls="navbar-test" aria-label="Toggle navigation" type="button" data-target="#navbar-test" data-toggle="collapse">
+                <span class="navbar-toggler-icon k-icon k-icon-md k-i-hamburger"></span>
+            </button>
+            <div class="navbar-collapse collapse show" id="navbar-test">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" id="dropdown1" aria-expanded="false" aria-haspopup="true" data-toggle="dropdown">Menu Item</a>
+                        <div class="dropdown-menu" aria-labelledby="dropdown1">
+                            <a class="dropdown-item" href="#">Sub menu</a>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Test</a>
+                    </li>
+                </ul>
+            </div>
+        </nav> -->
+    </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Home from './components/Home'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    components: {
+            Home,
+    }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+
+.vh-100 {
+    min-height: 100vh;
 }
+
+/* #wrapper {
+    background-color: rgba(0,0,255,.1);
+    width: 100% !important;
+    height: 100% !important;
+    min-height: 100% !important;
+    display: block !important;
+}  */
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,53 @@
+<template>
+    <div id="home" class="row" style="">
+        <main class="col-11 p-4 flex-fill">
+            <div class="row p-3">
+                <div class="col-6">
+                    <div class="p-3 border bg-light">Custom column padding</div>
+                </div>
+                <div class="col-6">
+                    <div class="p-3 border bg-light">Custom column padding</div>
+                </div>
+                <div class="col-6">
+                    <div class="p-3 border bg-light">Custom column padding</div>
+                </div>
+                <div class="col-6">
+                    <div class="p-3 border bg-light">Custom column padding</div>
+                </div>
+            </div>
+        </main>
+    </div>
+    
+
+        <!-- <div class="row flex-grow-1">
+            <main class="col p-4 flex-fill" style="background-color: rgba(0,0,255,.1);">
+                <h1>Main area</h1>
+                
+                <p>Sriracha biodiesel taxidermy organic post-ironic, Intelligentsia salvia mustache 90's code editing brunch. Butcher polaroid VHS art party, hashtag Brooklyn deep v PBR narwhal sustainable mixtape swag wolf squid tote bag. Tote bag cronut semiotics,
+                    raw denim deep v taxidermy messenger bag. Tofu YOLO Etsy, direct trade ethical Odd Future jean shorts paleo. Forage Shoreditch tousled aesthetic irony, street art organic Bushwick artisan cliche semiotics ugh synth chillwave meditation.
+                    Shabby chic lomo plaid vinyl chambray Vice. Vice sustainable cardigan, Williamsburg master cleanse hella DIY 90's blog.</p>
+            </main>
+        </div> -->
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  props: {
+    // msg: String
+  },
+  components: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+    #home {
+        background: #2c3e50;
+        /* min-width: 90%;
+        width: 90%; */
+    }
+</style>
