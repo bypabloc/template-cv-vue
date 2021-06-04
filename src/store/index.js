8a44e7ffd8ea88cd7de5487f18cd9dbb1a4b8147
Pablo Contreras <pacg1991@gmail.com>
2021-06-04 16:30:00 -0500

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -8,7 +8,6 @@ import getters from './getters'
 
 const debug = process.env.NODE_ENV !== 'production';
 
-// Create a new store instance.
 export default createStore({
     strict: debug,
     plugins: debug ? [createLogger()] : [],
